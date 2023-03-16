55fce308f757b20be96e1456cf34945fb2947d26
fatibrug <99653415+fatibrug@users.noreply.github.com>
2023-03-16 13:58:41 +0100

    Update index.js

--- produkt/index.js
+++ produkt/index.js
@@ -1,16 +1,13 @@
 
-
 const dropdown = document.getElementsByName('Artikelnummer')[0];
 const inputField = document.getElementsByName('Bezeichnung')[0];
 
 dropdown.addEventListener('change', (event) => {
     const selectedValue = event.target.value;
     const selectedOption = event.target.options[event.target.selectedIndex];
-    const selectedDescription = selectedOption.getAttribute('data-description');
+    const selectedDescription = selectedOption.getAttribute('Bezeichnung');
     inputField.value = selectedDescription;
 });
-
-
 
 
 
@@ -53,7 +50,6 @@ dropdown.addEventListener('change', (event) => {
         description = articles[selectedValue];
     }
     
-    // Update the input fields with the selected article and description
+    // Update 
     inputField.value = description;
 });
-
