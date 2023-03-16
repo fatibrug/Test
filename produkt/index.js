50c0612883a4801862ed92ac0007385faf0349e1
fatibrug <99653415+fatibrug@users.noreply.github.com>
2023-03-16 13:57:50 +0100

    Update index.js

--- produkt/index.js
+++ produkt/index.js
@@ -9,3 +9,51 @@ dropdown.addEventListener('change', (event) => {
     const selectedDescription = selectedOption.getAttribute('data-description');
     inputField.value = selectedDescription;
 });
+
+
+
+
+
+
+// oder du kannst es auch so mit der if bedingung probieren
+
+dropdown.addEventListener('change', (event) => {
+    const selectedValue = event.target.value;
+    let selectedDescription = '';
+    
+    if (selectedValue === '123') {
+        selectedDescription = 'This is the description for item 123.';
+    }
+    
+    inputField.value = selectedDescription;
+});
+
+
+
+//oder hier...
+
+const dropdown = document.getElementsByName('Artikelnummer')[0];
+const inputField = document.getElementsByName('Bezeichnung')[0];
+
+// keine Ahnung wo deine sachen definiert sind...
+const articles = {
+    '123': 'This is the description for item 123.',
+    '456': 'This is the description for item 456.',
+    '789': 'This is the description for item 789.'
+};
+
+dropdown.addEventListener('change', (event) => {
+    const selectedValue = event.target.value;
+    let article = '';
+    let description = '';
+    
+   
+    if (articles.hasOwnProperty(selectedValue)) {
+        article = selectedValue;
+        description = articles[selectedValue];
+    }
+    
+    // Update the input fields with the selected article and description
+    inputField.value = description;
+});
+
